4e5646ce08c05c4b67d502d5cc45f8bb03e2c218
vietanha74 <vietanha74@gmail.com>
2023-07-07 23:32:39 +0700

    Update Problem_4.py

--- Problem-4/Problem_4.py
+++ Problem-4/Problem_4.py
@@ -71,3 +71,40 @@ child.add_group(sub_child)
 parent.add_group(child)
 
 print(parent.user_exists(sub_child_user))
+
+#Test Case 1:
+# Create groups
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+# Add users
+parent.add_user("user1")
+child.add_user("user2")
+sub_child.add_user("user3")
+
+# Add groups
+child.add_group(sub_child)
+parent.add_group(child)
+
+# Check if user exists
+print(parent.user_exists("user3"))  # Expected output: True
+
+#Test Case 2:
+# Create group
+group = Group("group")
+
+# Check if user exists in an empty group
+print(group.user_exists("user1"))  # Expected output: False
+
+#Test Case 3:
+# Create group
+group = Group("group")
+
+# Add users
+group.add_user("user1")
+group.add_user("user2")
+group.add_user("user3")
+
+# Check if user exists
+print(group.user_exists("user2"))  # Expected output: True
